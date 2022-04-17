462795287fec2f1178e3555f7a7c7fec2807ac2f
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-04-17 14:25:29 +0600

    Home banner design done with reponsiveness

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import CustomLink from "../../CustomLInk/CustomLink";
+import './Header.module.css'
 // import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 const Header = () => {
@@ -11,7 +12,7 @@ const Header = () => {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
-                <Link className="navbar-brand" to="/">Heaven Dental Home</Link>
+                <Link className="navbar-brand" to="/">Heaven <span className="oranged">Dental</span> Home</Link>
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
